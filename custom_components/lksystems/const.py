8c0659b8a91498a84f33de2408dc2da13e525257
Anders Yderborg <anders.yderborg@gmail.com>
2025-10-06 11:13:26 +0200

    Readded Cubic sensors

--- custom_components/lksystems/const.py
+++ custom_components/lksystems/const.py
@@ -2,6 +2,12 @@
 
 from typing import Final
 
+from homeassistant.components.sensor import (
+    SensorDeviceClass,
+    SensorEntityDescription,
+    SensorStateClass,
+)
+
 DOMAIN = "lksystems"
 INTEGRATION_NAME = "LK Systems"
 ATTRIBUTION = "Data provided by LK Systems API"
@@ -20,3 +26,67 @@ CONF_UPDATE_INTERVAL = "update_interval"
 
 # Default update interval in minutes
 DEFAULT_UPDATE_INTERVAL = 5
+
+
+LK_CUBICSECURE_SENSORS: dict[str, SensorEntityDescription] = {
+    "volumetotalday": SensorEntityDescription(
+        key="volumeTotalDay",
+        name="Total Volume Day",
+        icon="mdi:water",
+        device_class=SensorDeviceClass.WATER,
+        unit_of_measurement="L",
+        native_unit_of_measurement="L",
+        state_class=SensorStateClass.TOTAL,
+        translation_key="volume_total_day_sensor",
+    ),
+    "volumetotal": SensorEntityDescription(
+        key="volumeTotal",
+        name="Total Volume",
+        icon="mdi:water",
+        device_class=SensorDeviceClass.WATER,
+        unit_of_measurement="L",
+        native_unit_of_measurement="L",
+        state_class=SensorStateClass.TOTAL,
+        translation_key="volume_total_sensor",
+    ),
+    "tempWaterAverage": SensorEntityDescription(
+        key="tempWaterAverage",
+        name="Average Water Temperature",
+        icon="mdi:thermometer",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        unit_of_measurement="°C",
+        native_unit_of_measurement="°C",
+        state_class=SensorStateClass.MEASUREMENT,
+        translation_key="temp_water_average_sensor",
+    ),
+    "tempWaterMin": SensorEntityDescription(
+        key="tempWaterMin",
+        name="Min Water Temperature",
+        icon="mdi:thermometer",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        unit_of_measurement="°C",
+        native_unit_of_measurement="°C",
+        state_class=SensorStateClass.MEASUREMENT,
+        translation_key="temp_water_min_sensor",
+    ),
+    "tempWaterMax": SensorEntityDescription(
+        key="tempWaterMax",
+        name="Max Water Temperature",
+        icon="mdi:thermometer",
+        device_class=SensorDeviceClass.TEMPERATURE,
+        unit_of_measurement="°C",
+        native_unit_of_measurement="°C",
+        state_class=SensorStateClass.MEASUREMENT,
+        translation_key="temp_water_max_sensor",
+    ),
+    "waterPressure": SensorEntityDescription(
+        key="waterPressure",
+        name="Water Pressure",
+        icon="mdi:gauge-low",
+        device_class=SensorDeviceClass.PRESSURE,
+        unit_of_measurement="hPa",
+        native_unit_of_measurement="hPa",
+        state_class=SensorStateClass.MEASUREMENT,
+        translation_key="water_pressure_sensor",
+    ),
+}
